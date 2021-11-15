d6a864bd9b70365e3900a04862a58dc619f6e47a
bozimmerman <bo@zimmers.net>
2021-11-15 08:05:05 +0000

    More hostcm progress
    git-svn-id: svn://192.168.1.10/public/Zimodem@21124 0d6f1817-ed0e-0410-87c9-987e46238f29

--- zimodem/proto_hostcm.h
+++ zimodem/proto_hostcm.h
@@ -1,5 +1,23 @@
+#ifdef INCLUDE_SD_SHELL
 /* Converted from source reverse engineered from SP9000 roms by Rob Ferguson */
 #include <FS.h>
+
+# define HCM_BUFSIZ  104
+# define HCM_SENDBUF (208/2 - 6)
+# define HCM_FNSIZ 32
+# define HCM_MAXFN 16
+
+typedef struct _HCMFile
+{
+  char descriptor;
+  File f;
+  uint8_t mode;
+  uint8_t format;
+  uint8_t type;
+  int reclen;
+  char filename[HCM_FNSIZ+1];
+  struct _HCMFile *nxt;
+} HCMFile;
 
 class HostCM
 {
@@ -8,7 +26,7 @@ private:
   const struct _HCOpts
   {
     unsigned int  speed = 15; //B9600
-    unsigned int  parity;
+    unsigned int  parity = 0;// ?!
     unsigned int  stopb=0;
     unsigned char lineend=0xd;
     unsigned char prompt=0x11;
@@ -16,27 +34,37 @@ private:
     unsigned char ext=0;
   } opt PROGMEM;
 
-# define HCM_BUFSIZ  128
-# define HCM_SENDBUF (208/2 - 6)
-  const char *sumchar PROGMEM = "ABCDEFGHIJKLMNOP";
-
   uint8_t lastOutBuf[HCM_BUFSIZ];
   int lastOutSize = 0;
 
-  uint8_t inbuf[HCM_BUFSIZ];
+  uint8_t inbuf[HCM_BUFSIZ+1];
   int pkti = 0;
 
   bool aborted = false;
   unsigned long lastNonPlusTm = 0;
   unsigned int plussesInARow = 0;
   unsigned long plusTimeExpire = 0;
+  HCMFile files[HCM_MAXFN];
+  FS *hFS = &SD;
 
   char checksum(uint8_t *b, int n);
   void checkDoPlusPlusPlus(const int c, const unsigned long tm);
   bool checkPlusPlusPlusExpire(const unsigned long tm);
   void sendNAK();
   void sendACK();
+  void sendError(const char* format, ...);
+  void closeAllFiles();
+  HCMFile *addNewFileEntry();
+  void delFileEntry(HCMFile *e);
+  HCMFile *getFileByDescriptor(char c);
+  int numOpenFiles();
+
+  void protoOpenFile();
+  void protoCloseFile();
 public:
   void receiveLoop();
   bool isAborted();
+  HostCM(FS *fs);
+  ~HostCM();
 };
+#endif
--- zimodem/proto_hostcm.ino
+++ zimodem/proto_hostcm.ino
@@ -1,5 +1,71 @@
+#ifdef INCLUDE_SD_SHELL
 /* Converted from source reverse engineered from SP9000 roms by Rob Ferguson */
 #include "proto_hostcm.h"
+#include <libgen.h>
+
+HostCM::HostCM(FS *fs)
+{
+  hFS = fs;
+  for(int i=0;i<HCM_MAXFN;i++)
+  {
+    files[i].f = (File)0;
+    files[i].descriptor = 'A' + i;
+    delFileEntry(&files[i]);
+  }
+}
+
+HostCM::~HostCM()
+{
+  closeAllFiles();
+}
+
+
+void HostCM::closeAllFiles()
+{
+  for(int i=0;i<HCM_MAXFN;i++)
+    delFileEntry(&files[i]);
+}
+
+HCMFile *HostCM::addNewFileEntry()
+{
+  for(int i=0;i<HCM_MAXFN;i++)
+  {
+    if(files[i].f == 0)
+      return &files[i];
+  }
+  return 0;
+}
+
+void HostCM::delFileEntry(HCMFile *e)
+{
+  if(e->f != 0)
+    e->f.close();
+  char d = e->descriptor;
+  memset(e,sizeof(struct _HCMFile),0);
+  e->descriptor = d;
+}
+
+
+HCMFile *HostCM::getFileByDescriptor(char c)
+{
+  for(int i=0;i<HCM_MAXFN;i++)
+  {
+    if(files[i].descriptor == c)
+      return &files[i];
+  }
+  return 0;
+}
+
+int HostCM::numOpenFiles()
+{
+  int n=0;
+  for(int i=0;i<HCM_MAXFN;i++)
+  {
+    if(files[i].f != 0)
+      n++;
+  }
+  return n;
+}
 
 void HostCM::checkDoPlusPlusPlus(const int c, const unsigned long tm)
 {
@@ -40,14 +106,153 @@ bool HostCM::isAborted()
   return aborted;
 }
 
+void HostCM::protoCloseFile()
+{
+  int findex = inbuf[1] - 'A';
+  if ((findex >= HCM_MAXFN) || (findex < 0)) {
+    sendError("error: invalid descriptor %c", inbuf[1]));
+    return;
+  }
+  
+  if (files[findex].f == 0)
+  {
+    sendError("error: file not open %c", inbuf[1])); // should this be an error though?
+    return;
+  }
+  delFileEntry(&files[findex]);
+  sendAck();
+}
+
+void HostCM::protoOpenFile()
+{
+  uint8_t *eobuf = inbuf + pkti;
+  int n=numOpenFiles();
+  if(n >= HCM_MAXFN)
+  {
+    sendError("error: too many open files");
+    return;
+  }
+  if(pkti<8)
+  {
+    sendError("error: command too short");
+    return;
+  }
+  uint8_t mode = inbuf[1];
+  if(strchr("rlwsuaRLWSUA",(char)mode)==0)
+  {
+    sendError("error: illegal mode %c",(char)mode);
+    return;
+  }
+  bool isRead = strchr("RLrl",(char)mode)!=0;
+  uint8_t format = inbuf[2];
+  uint8_t *ptr = (uint8_t *)memchr(inbuf+3, '(', pkti-3);
+  if(ptr == 0)
+  {
+    sendError("error: missing (");
+    return;
+  }
+  uint8_t type = ptr[1];
+  uint8_t reclen = 0;
+  if(ptr[2]==':')
+    reclen=atoi((char *)(ptr+3));
+  ptr = (uint8_t *)memchr(ptr+2, ')', eobuf-(ptr+1));
+  if(ptr == 0)
+  {
+    sendError("error: missing )");
+    return;
+  }
+  inbuf[pkti - 1] = 0; 
+  uint8_t *fnptr = ptr + 1;
+  HCMFile *newF = addNewFileEntry();
+  if (strchr("fF",(char)type) != 0)
+  {
+    char *bn = basename((char *)ptr+1);
+    char *dn = dirname((char *)ptr+1);
+    if(reclen == 0)
+      reclen = 80;
+    snprintf(newF->filename, sizeof(newF->filename), "%s/(f:%d)%s", dn, reclen, bn);
+  } 
+  else
+    strncpy(newF->filename, (char *)ptr+1, sizeof(newF->filename));
+  if(isRead)
+  {
+    newF->f = SD.open(newF->filename);
+    if(newF->f == 0)
+    {
+      if (strchr(basename(newF->filename), ',') == 0) 
+      {
+        if (strlen(newF->filename) + 5 < HCM_FNSIZ) 
+        {
+          if (strchr("lsLS",(char)mode)!=0) 
+            strcat(newF->filename, ",prg");
+          else
+          {
+            if (strchr("fF",(char)type) != 0)
+              strcat(newF->filename, ",rel");
+            else
+              strcat(newF->filename, ",seq");  
+          }
+          newF->f = SD.open(newF->filename);
+          if(newF == 0)
+          {
+            sendError("error: file '%s' not found", newF->filename);
+            return;
+          }
+        } 
+      } 
+      else 
+      { 
+        sendError("error: file '%s' not found", newF->filename);
+        return;
+      }
+    }
+  }
+  else
+  {
+    newF->f = SD.open(newF->filename,FILE_WRITE);
+    if(newF->f && (strchr("aA",(char)mode)!=0))
+      newF->f.seek(EOF);
+  }
+  newF->mode = mode;
+  newF->format = format;
+  newF->type = type;
+  newF->reclen = reclen;
+  
+  lastOutSize = 0;
+  lastOutBuf[lastOutSize++] = opt.response;
+  lastOutBuf[lastOutSize++] = 'b';
+  lastOutBuf[lastOutSize++] = newF->descriptor;
+  lastOutBuf[lastOutSize++] = checksum(&(lastOutBuf[1]), 2);
+  lastOutBuf[lastOutSize++] = opt.lineend;
+  lastOutBuf[lastOutSize++] = opt.prompt;
+  hserial.write(lastOutBuf, lastOutSize);
+  hserial.flush();
+}
+
 char HostCM::checksum(uint8_t *b, int n)
 {
   int i, s = 0;
 
-  for (i = 0; i < n; i++) {
+  for (i = 0; i < n; i++)
     s += b[i] & 0xf;
-  }
-  return (sumchar[s&0xf]);
+  return ('A' + (s&0xf));
+}
+
+void HostCM::sendError(const char* format, ...)
+{
+  lastOutSize = 0;
+  lastOutBuf[lastOutSize++] = opt.response;
+  lastOutBuf[lastOutSize++] = 'x';
+  va_list arglist;
+  va_start(arglist, format);
+  lastOutSize += vsnprintf((char *)&lastOutBuf[2], 80,  format, arglist); 
+  va_end(arglist);
+  lastOutBuf[lastOutSize] = checksum(&lastOutBuf[1], lastOutSize - 1);
+  lastOutSize++;
+  lastOutBuf[lastOutSize++] = opt.lineend;
+  lastOutBuf[lastOutSize++] = opt.prompt;
+  hserial.write(lastOutBuf, lastOutSize);
+  hserial.flush();
 }
 
 void HostCM::sendNAK()
@@ -55,10 +260,6 @@ void HostCM::sendNAK()
   lastOutSize = 0;
   lastOutBuf[lastOutSize++] = opt.response;
   lastOutBuf[lastOutSize++] = 'N';
-
-  /* NAK does not to include checksum */  
-/*      outbuf[count++] = checksum(&(outbuf[1]), 1);  */ 
-
   lastOutBuf[lastOutSize++] = opt.lineend;
   lastOutBuf[lastOutSize++] = opt.prompt;
   hserial.write(lastOutBuf, lastOutSize);
@@ -124,14 +325,14 @@ void HostCM::receiveLoop()
         sendACK();
         break;
       case 'q':
-        //TODO: close all open files
+        closeAllFiles();
         aborted=true;
         break;
       case 'o':
-        //TODO: count = hostopen(inbuf, n, outbuf);
+        protoOpenFile();
         break;
       case 'c':
-        //TODO: count = hostclose(inbuf, n, outbuf);
+        protoCloseFile();
         break;
       case 'p':
         //TODO: count = hostput(inbuf, n, outbuf);
@@ -168,3 +369,4 @@ void HostCM::receiveLoop()
   pkti=0; // we are ready for the next packet!
   serialOutDeque();
 }
+#endif
